ab415188f3d6bf04dfc8225ff1051cfceceddbf1
ketelol <olivier.ketels@gmail.com>
2017-11-17 09:40:19 +0100

    Initial project setup.

---
+++ src/main/java/be/xplore/dockerci/DockerciApplication.java
@@ -0,0 +1,12 @@
+package be.xplore.dockerci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DockerciApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DockerciApplication.class, args);
+	}
+}
